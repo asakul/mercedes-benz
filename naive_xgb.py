dfd1d3d3777d189f9cd5b5118fad3cc7352076f7
niktarasov <nik.tarasov@gmail.com>
2017-06-30 01:46:08 +0100

    Initial commit

---
+++ naive_xgb.py
@@ -0,0 +1,50 @@
+""" This is a simply naive XGBoost model. Python3"""
+
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import os
+import matplotlib.pyplot as plt
+
+DATA_DIR = 'data'
+TRAIN_FILE_PATH = os.path.join(DATA_DIR, 'train.csv')
+TEST_FILE_PATH = os.path.join(DATA_DIR, 'test.csv')
+SAMPLE_SUBMISSION_FILE_PATH = os.path.join(DATA_DIR, 'sample_submission.csv')
+
+raw_train_data = pd.read_csv(TRAIN_FILE_PATH)
+raw_test_data = pd.read_csv(TEST_FILE_PATH)
+
+# print(raw_train_data.head(5))
+
+
+train_data = raw_train_data.copy()
+test_data = raw_test_data.copy()
+
+
+# a bloc of code for prediction. Need to add feature engineering above and remove 'Xn' from .drop()
+
+xgb_params = {
+    'eta': 0.01,
+    'max_depth': 7,
+    'subsample': 0.8,
+    'lambda': 8,
+    'alpha': 2,
+    'colsample_bytree': 0.9,
+    'colsample_bylevel': 0.9,
+    'objective': 'reg:linear',
+    'eval_metric': 'rmse',
+    'silent': 1,
+    'nthread': 8
+}
+
+dtrain = xgb.DMatrix(train_data.drop(['ID','X0', 'X1','X2','X3','X4','X5','X6', 'X8', 'y'], axis=1), train_data['y'])
+
+dtest = xgb.DMatrix(test_data.drop(['ID','X0', 'X1','X2','X3','X4','X5','X6', 'X8'], axis=1))
+
+cv_output = xgb.cv(
+    xgb_params, dtrain, num_boost_round=5000, early_stopping_rounds=50,
+    verbose_eval=100, show_stdv=False
+)
+
+cv_output[['train-rmse-mean', 'test-rmse-mean']].plot()
+plt.show()
